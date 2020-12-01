8da0611c1fa18573db57b49461641215487cdc8c
vinit-churi <vinitchuri@hotmail.com>
2020-12-01 16:06:28 +0530

    need to use context

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import { Header } from "../header/Header";
+import { Balance } from "../balance/Balance";
+import { IncomeExpenses } from "../incomeExpenses/IncomeExpenses";
+import { TransactionList } from "../transactionList/TransactionList";
+import { AddTransaction } from "../addtransaction/AddTransaction";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
---
+++ src/components/balance/Balance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div>
+            <>
+                <h2>
+                    Your Balance
+                </h2>
+                <h1 id="balance">$0.00</h1>
+            </>
+        </div>
+    )
+}
